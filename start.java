8b54a4f35400f2ff9153fac6d769e69244fe9409
Hajonghyeon <jhjm5958@naver.com>
2022-03-30 09:43:22 +0900

    시작

---
+++ start.java
@@ -0,0 +1,13 @@
+public class start { // 1
+
+	public static void main(String[] args) { // 2
+		// 명령이 시작되는 부분 -> main 메소드
+		
+		// 출력을 위한 명령 진행하기!
+		// 한 줄 명령이 끝났을 때 꼭 ; 붙여주기!
+		System.out.println("자바 시작!");
+		
+		
+	}
+
+}
