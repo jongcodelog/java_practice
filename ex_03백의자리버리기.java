74ae192cd19fd3a21873d4c4b0336565019335ec
Hajonghyeon <jhjm5958@naver.com>
2022-04-07 09:46:05 +0900

    백의자리제외하고 나머지 버리기

---
+++ ex_03백의자리버리기.java
@@ -0,0 +1,9 @@
+public class ex_03백의자리버리기 {
+    public static void main(String[] args) {
+
+      // 백의자리를 제외하고 나머지 값 버리는 결과 출력
+      int a = 456;
+      int result = a/100*100;
+      System.out.println("결과 확인 : "+result);  
+    }
+}
