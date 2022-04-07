4c4965313e86e36f78fdce718c60de0c83c970ef
Hajonghyeon <jhjm5958@naver.com>
2022-04-07 09:42:21 +0900

    4가지연산

---
+++ ex_02operator.java
@@ -0,0 +1,20 @@
+public class ex_02operator {
+    public static void main(String[] args) {
+
+    
+    int num1 = 9;
+    int num2 = 5;
+
+    int sum = num1+num2;
+    int minus = num1-num2;
+    int mul = num1*num2;
+    int div = num1/num2;
+
+    System.out.println("더하기 : "+sum);
+    System.out.println("빼기 : "+minus);
+    System.out.println("곱하기 : "+mul);
+    System.out.println("나누기 : "+div);
+
+    }
+
+}
