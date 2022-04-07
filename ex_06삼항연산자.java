93ce11a5acd01b9c22135dbd1d4ca50f821d2ee0
Hajonghyeon <jhjm5958@naver.com>
2022-04-07 10:25:36 +0900

    삼항연산자 기초

---
+++ ex_06삼항연산자.java
@@ -0,0 +1,10 @@
+public class ex_06삼항연산자 {
+    public static void main(String[] args) {
+    int num1 = 10;
+    int num2 = 20;
+
+    String result = (num1==num2) ? "같다" : "다르다";
+    System.out.println("결과 : "+result);
+    
+    }
+}
